3ca80eea983e59c46bbb27a73cd0b75d92c77877
faridhva22-jpg <faridhva22@gmail.com>
2025-12-29 20:31:50 +0100

    Update ct-total-supply.js

--- api/api/ct-total-supply.js
+++ api/api/ct-total-supply.js
@@ -1,1 +1,32 @@
+export default async function handler(req, res) {
+  try {
+    const rpcUrl = "https://api.mainnet-beta.solana.com";
+    const mint = "9YZZtRDqd9kiJMqgZ8XmFaE1mfK2T4kNpYgW1Nfn9rYV";
+
+    const body = {
+      jsonrpc: "2.0",
+      id: 1,
+      method: "getTokenSupply",
+      params: [mint]
+    };
+
+    const rpcRes = await fetch(rpcUrl, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(body)
+    });
+
+    const json = await rpcRes.json();
+    const uiAmount = json.result?.value?.uiAmount;
+
+    if (typeof uiAmount !== "number") {
+      return res.status(500).send("0");
+    }
+
+    res.setHeader("Content-Type", "text/plain");
+    return res.send(uiAmount.toString());
+  } catch (e) {
+    return res.status(500).send("0");
+  }
+}
 
